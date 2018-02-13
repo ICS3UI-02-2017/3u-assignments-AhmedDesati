c3b56794a32f408d16328cae82686371c4212959
Desaa3902 <Desaa3902@HRH-IC0027322.wrdsb.ca>
2018-02-13 11:09:04 -0500

    finished a1q1

--- src/Assignment1/A1Q1.java
+++ src/Assignment1/A1Q1.java
@@ -5,6 +5,11 @@
  */
 package Assignment1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /**
  *
  * @author lamon
@@ -15,7 +20,41 @@ public class A1Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+
+        
+//create a new city for the robot
+        City kw = new City();
+
+//Put a new robot in kw   
+
+        RobotSE arnold = new RobotSE(kw, 0, 2, Direction.WEST);
+
+//put a new set walls in the city kw
+
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.EAST);
+        new Wall(kw, 1, 2, Direction.EAST);
+
+// tell the robot to go around the box of walls  
+    
+        arnold.move(2);
+        arnold.turnLeft();
+        arnold.move(3);
+        arnold.turnLeft();
+        arnold.move(3);
+        arnold.turnLeft();
+        arnold.move(3);
+        arnold.turnLeft();
+        arnold.move();
+    
+    
+    
     }
     
 }
